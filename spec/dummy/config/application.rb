64c52f523ae0abc72aa6223145c5d853d3317034
Matt Rideout <mrideout@windserve.com>
2025-12-06 15:21:25 -0500

    Use Rails 8.1 configuration defaults

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,8 +7,8 @@ require "payola"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for Rails 8.0
-    config.load_defaults 8.0
+    # Initialize configuration defaults for Rails 8.1
+    config.load_defaults 8.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
